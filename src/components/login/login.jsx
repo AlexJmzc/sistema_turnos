68c81f0ac10464d0a63854ee9db3a869fe198ee2
AlexJmzc <73499509+AlexJmzc@users.noreply.github.com>
2023-09-23 17:11:51 -0500

    Numero de ventanillas

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,19 +1,26 @@
 import React, { useState, useEffect }from 'react';
 import logo from '../../assets/img/Logo.png';
 import './login.css';
-import { useValue } from '../contexto';
 import { useNavigate } from 'react-router';
 import axios from 'axios';
-import { urlUsuarios } from '../../api/urls';
-
+import { urlUsuarios, urlSucursales } from '../../api/urls';
 
 const Login = () => {
+  //? Sucursal
+  const [ selectedValue ] = localStorage.getItem("sucursal");
+  const [sucursal, setSucursal] = useState();
+  const [ventanilla, setVentanilla] = useState(0);
+
+  const [arrayNumeros, setArray] = useState([]);
+
   //URL API
   const apiUrl = urlUsuarios.login;
+  const apiUrlSucursal = urlSucursales.obtenerSucursal + selectedValue;
 
   //Logeo
   const [nombre, setNombre] = useState('');
   const [clave, setClave] = useState('');
+
 
   //Usuario
   const [usuario, setUsuario] = useState([]);
@@ -27,7 +34,29 @@ const Login = () => {
       navigate("/");
     }
 
-  }, [])
+  }, []);
+
+  //! CARGA LA SUCURSAL ACTUAL
+  useEffect(() => {
+    axios
+      .get(apiUrlSucursal)
+      .then((response) => {
+        setSucursal(response.data);
+        setArray(array(response.data.Numero_Ventanillas));
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [sucursal, apiUrlSucursal]);
+
+  const array = (n) => {
+    const num = [];
+    for (let i = 1; i <= n; i++) {
+      num.push(i);
+    }
+
+    return num;
+  }
 
   const nombreChange = (e) => {
     setNombre(e.target.value);
@@ -37,9 +66,13 @@ const Login = () => {
     setClave(e.target.value);
   }
 
+  //!Metodo para seleccionar sucursal
+  const cambioVentanilla = (event) => {
+    const value = event.target.value;
+    setVentanilla(value);
+    localStorage.setItem("ventanilla", value);
+  };
 
-  //Contexto
-  const { selectedValue } = localStorage.getItem("sucursal");
 
   //Navegacion
   const navigate = useNavigate();
@@ -51,13 +84,24 @@ const Login = () => {
     const user = response.data;
 
     if(user.Clave != null) {
-      setUsuario(user);
-      localStorage.setItem("user", JSON.stringify(user.ID_Usuario));
-      localStorage.setItem("token", JSON.stringify(user.Token));
-      localStorage.setItem("rol", JSON.stringify(user.ID_Rol));
-      Redireccion(user.ID_Rol);
+      if(user.ID_Rol === 1) {
+        setUsuario(user);
+        localStorage.setItem("user", JSON.stringify(user.ID_Usuario));
+        localStorage.setItem("token", JSON.stringify(user.Token));
+        localStorage.setItem("rol", JSON.stringify(user.ID_Rol));
+        Redireccion(user.ID_Rol);
+      } else if(user.ID_Rol !== 1 && ventanilla === 0) {
+        alert("Selecciona la ventanilla");
+      } else {
+        setUsuario(user);
+        localStorage.setItem("user", JSON.stringify(user.ID_Usuario));
+        localStorage.setItem("token", JSON.stringify(user.Token));
+        localStorage.setItem("rol", JSON.stringify(user.ID_Rol));
+        Redireccion(user.ID_Rol);
+      }
+        
     } else {
-      alert('Usuario o contraseña incorrectos');
+        alert('Usuario o contraseña incorrectos');
     }
   }
 
@@ -88,6 +132,15 @@ const Login = () => {
                 <input type="text" placeholder='Usuario' onChange={nombreChange}/>
 
                 <input type="password" placeholder='Clave' onChange={claveChange}/>
+
+                <select id="ventanilla" onChange={cambioVentanilla}>
+                  <option value='0'>Selecciona la ventanilla</option>
+                  {arrayNumeros.map((numero) => (
+                    <option key={numero} value={numero}>
+                      {numero}
+                    </option>
+                  ))}
+                </select>
                 
                 <button className='Btn' onClick={Logeo}>Login</button>
                 
